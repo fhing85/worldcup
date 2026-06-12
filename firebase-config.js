ccd9eaa22f98ec1bf98ee1e3308e6dee8663c189
fhing85 <fhing@snu.ac.kr>
2026-06-12 19:00:25 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDpcWizWBlo8mjIoBJanR7plYRSXKaFbY8",
   authDomain: "worldcup-e4350.firebaseapp.com",
   databaseURL:
-    "https://worldcup-e4350-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "https://worldcup-e4350-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "worldcup-e4350",
   appId: "1:152597752170:web:9b9bb26aebda28cef01d13",
 };
