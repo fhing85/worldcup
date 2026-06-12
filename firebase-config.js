40dbe5b997cb1fcc70910f42928c3f9c2b1523e1
fhing85 <fhing@snu.ac.kr>
2026-06-12 18:47:10 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Firebase 설정 후 아래 값을 교체하세요.
 // 설정 전에는 브라우저의 로컬 저장소를 사용하는 데모 모드로 실행됩니다.
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  apiKey: "AIzaSyDpcWizWBlo8mjIoBJanR7plYRSXKaFbY8",
+  authDomain: "worldcup-e4350.firebaseapp.com",
   databaseURL:
-    "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID",
+    "https://worldcup-e4350-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "worldcup-e4350",
+  appId: "1:152597752170:web:9b9bb26aebda28cef01d13",
 };
